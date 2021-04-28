60ff58d1b7e1b864b937bda9426d3fec50186a90
Thuy Ng <thuypn9a4@gmail.com>
2021-04-28 12:41:12 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,5 @@ setuptools.setup(
 	name="cvut",
 	version='0.0.1',
 	packages=setuptools.find_packages(),
-	install_requires=[
-		'numpy', 'mmcv', 'opencv-python',
-	],
 	python_requires='>=3.6',
 )
