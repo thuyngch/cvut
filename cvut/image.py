ed6b94ec5bf0d826a723ea5ccebd70d8cc8afb3f
AntiAegis <thuypn9a4@gmail.com>
2021-09-18 18:01:04 +0700

    add glob_folders

--- cvut/image.py
+++ cvut/image.py
@@ -4,7 +4,7 @@ import numpy as np
 from glob import glob
 
 
-__all__ = ["impt2np", "imdenormalize", "imwrite", "glob_imgs"]
+__all__ = ["impt2np", "imdenormalize", "imwrite", "glob_imgs", "glob_folders"]
 
 SUPPORT_IMG_FORMATS = ['jpg', 'jpeg', 'png']
 
@@ -20,7 +20,10 @@ def impt2np(image):
 # ------------------------------------------------------------------------------
 #  imdenormalize
 # ------------------------------------------------------------------------------
-def imdenormalize(image, mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], scale_255=False):
+def imdenormalize(image,
+                  mean=[123.675, 116.28, 103.53],
+                  std=[58.395, 57.12, 57.375],
+                  scale_255=False):
     if scale_255:
         image = np.clip(((image * std + mean) * 255.), 0, 255).astype('uint8')
     else:
@@ -52,3 +55,15 @@ def glob_imgs(img_dir, num_imgs=-1, recursive=False):
     if (num_imgs is not None) and (num_imgs != -1):
         img_files = img_files[:num_imgs]
     return img_files
+
+
+# ------------------------------------------------------------------------------
+#  glob_folders
+# ------------------------------------------------------------------------------
+def glob_folders(root_dir, recursive=False):
+    if not recursive:
+        folders = sorted(glob(os.path.join(root_dir, "**")))
+    else:
+        folders = sorted(glob(
+            os.path.join(root_dir, "**/**"), recursive=True))
+    return folders
