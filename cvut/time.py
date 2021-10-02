b5e60b55b47cd49524303ff80e3c1997d7398033
AntiAegis <thuypn9a4@gmail.com>
2021-10-02 17:23:31 +0700

    add more time formats

--- cvut/time.py
+++ cvut/time.py
@@ -1,24 +1,35 @@
 import time
 from datetime import datetime
 
-__all__ = ["TIME_FMT", "get_time_now", "str2time", "Timer"]
+__all__ = ["TIME_FMT_SEC", "TIME_FMT_DATE", "TIME_FMT_MIC",
+           "TIME_FMT_SEC_TZ", "TIME_FMT_DATE_TZ", "TIME_FMT_MIC_TZ",
+           "get_time_now", "str2time", "time2str", "Timer"]
 
 
 # ------------------------------------------------------------------------------
 #  Utils
 # ------------------------------------------------------------------------------
-TIME_FMT = "%Y-%m-%d-%H-%M-%S"
+TIME_FMT_SEC = "%Y-%m-%d-%H-%M-%S"
+TIME_FMT_DATE = "%Y-%m-%d"
+TIME_FMT_MIC = "%Y-%m-%d-%H-%M-%S-%f"
+TIME_FMT_SEC_TZ = "%Y-%m-%d-%H-%M-%S-%Z"
+TIME_FMT_DATE_TZ = "%Y-%m-%d-%Z"
+TIME_FMT_MIC_TZ = "%Y-%m-%d-%H-%M-%S-%f-%Z"
 
 
-def get_time_now(fmt=TIME_FMT, to_str=True):
+def get_time_now(fmt=TIME_FMT_SEC, to_str=True):
     cur_time = datetime.now()
     if to_str:
         cur_time = cur_time.strftime(fmt)
     return cur_time
 
 
-def str2time(str_time, fmt=TIME_FMT):
+def str2time(str_time, fmt=TIME_FMT_SEC):
     return datetime.strptime(str_time, fmt)
+
+
+def time2str(time_obj, fmt=TIME_FMT_SEC):
+    return time_obj.strftime(fmt)
 
 
 # ------------------------------------------------------------------------------
