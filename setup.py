a94f984ec47d94b3737bd11891b700a2184742aa
AntiAegis <thuypn9a4@gmail.com>
2022-03-05 07:41:16 +0700

    add install_requires

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 import setuptools
 
 setuptools.setup(
-	name="cvut",
-	version='0.0.1',
-	packages=setuptools.find_packages(),
-	python_requires='>=3.6',
+    name="cvut",
+    version='0.0.2',
+    packages=setuptools.find_packages(),
+    install_requires=['shapely'],
+    python_requires='>=3.6',
 )
