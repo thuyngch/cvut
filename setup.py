213d8b29f96d5da2be19f63f60f02195995d0bf5
Thuy Ng <thuypn9a4@gmail.com>
2022-03-07 14:53:01 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     name="cvut",
     version='0.0.2',
     packages=setuptools.find_packages(),
-    install_requires=['shapely', 'tqdm'],
+    install_requires=['shapely', 'tqdm', 'pycocotools'],
     entry_points={
         "console_scripts": [
             "video2images=cvut.tools.video2images:main",
