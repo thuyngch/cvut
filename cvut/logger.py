56829d8dfdd523cb4e00de4decaa6941aba698ef
TuanTNG <tangngoctuantttt@gmail.com>
2022-12-19 19:46:39 -0800

    Update logger.py

--- cvut/logger.py
+++ cvut/logger.py
@@ -3,6 +3,7 @@
 # ------------------------------------------------------------------------------
 import os
 import cv2
+import time
 import logging
 import numpy as np
 from logging.handlers import TimedRotatingFileHandler
@@ -21,6 +22,56 @@ def basename(filepath, wo_fmt=False):
         bname = '.'.join(bname.split('.')[:-1])
     return bname
 
+class CustomTimedRotatingFileHandler(TimedRotatingFileHandler):
+    def doRollover(self):
+        """
+        do a rollover; in this case, a date/time stamp is appended to the filename
+        when the rollover happens.  However, you want the file to be named for the
+        start of the interval, not the current time.  If there is a backup count,
+        then we have to get a list of matching filenames, sort them and remove
+        the one with the oldest suffix.
+        """
+        if self.stream:
+            self.stream.close()
+            self.stream = None
+        # get the time that this sequence started at and make it a TimeTuple
+        currentTime = int(time.time())
+        dstNow = time.localtime(currentTime)[-1]
+        t = self.rolloverAt - self.interval
+        if self.utc:
+            timeTuple = time.gmtime(t)
+        else:
+            timeTuple = time.localtime(t)
+            dstThen = timeTuple[-1]
+            if dstNow != dstThen:
+                if dstNow:
+                    addend = 3600
+                else:
+                    addend = -3600
+                timeTuple = time.localtime(t + addend)
+        dfn = self.rotation_filename(self.baseFilename + "." +
+                                     time.strftime("%Y-%m-%d_%H-%M-%S", timeTuple))
+        if os.path.exists(dfn):
+            os.remove(dfn)
+        self.rotate(self.baseFilename, dfn)
+        if self.backupCount > 0:
+            for s in self.getFilesToDelete():
+                os.remove(s)
+        if not self.delay:
+            self.stream = self._open()
+        newRolloverAt = self.computeRollover(currentTime)
+        while newRolloverAt <= currentTime:
+            newRolloverAt = newRolloverAt + self.interval
+        #If DST changes and midnight or weekly rollover, adjust for this.
+        if (self.when == 'MIDNIGHT' or self.when.startswith('W')) and not self.utc:
+            dstAtRollover = time.localtime(newRolloverAt)[-1]
+            if dstNow != dstAtRollover:
+                if not dstNow:  # DST kicks in before next rollover, so we need to deduct an hour
+                    addend = -3600
+                else:           # DST bows out before next rollover, so we need to add an hour
+                    addend = 3600
+                newRolloverAt += addend
+        self.rolloverAt = newRolloverAt
 
 # ------------------------------------------------------------------------------
 #   Logger
@@ -42,10 +93,8 @@ class Logger(logging.Logger):
 
         if logdir is not None:
             logfile = os.path.join(logdir, "%s.log" % (logname))
-            filehandler = TimedRotatingFileHandler(
+            filehandler = CustomTimedRotatingFileHandler(
                 logfile, when=when, backupCount=backupCount)
-            self.suffix = "%Y-%m-%d_%H-%M-%S"
-            self.extMatch = r"^\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}(\.\w+)?$"
             filehandler.setLevel(logging.INFO)
             filehandler.setFormatter(formatter)
 
