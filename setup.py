319e408a0dc04f289fdb9b08b5cbc593e675860e
thuyngch <thuypn9a4@gmail.com>
2023-02-28 09:38:52 +0700

    update version 0.1.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="cvut",
-    version='0.1.5',
+    version='0.1.6',
     packages=setuptools.find_packages(),
     entry_points={
         "console_scripts": [
