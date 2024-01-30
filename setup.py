0699452584dd1dd225935e59b4e0cc969cb00833
Thuy Ng <thuypn9a4@gmail.com>
2024-01-30 08:10:03 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="cvut",
-    version='0.1.14',
+    version='0.1.15',
     packages=setuptools.find_packages(),
     entry_points={
         "console_scripts": [
